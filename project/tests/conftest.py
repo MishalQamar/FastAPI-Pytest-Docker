e53c3e557b314100979226f5b83bcea19fa88db4
Mishal Qamar <mishalqamar@gmail.com>
2026-02-01 17:47:32 +0000

    Add summaries tests and test_app_with_db fixture

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -4,7 +4,8 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.config import Settings, get_settings
-from app import main
+from app.main import create_application
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -14,6 +15,24 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+        # testing
         yield test_client
